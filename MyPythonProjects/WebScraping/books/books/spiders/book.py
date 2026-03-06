867f906118ca8b0cac779521155ef55ab1d44eff
Alin044 <alinalin03030@gmail.com>
2026-03-06 12:04:44 +0000

    book item

---
+++ MyPythonProjects/WebScraping/books/books/spiders/book.py
@@ -0,0 +1,11 @@
+import scrapy
+
+from books.items import BooksItem
+
+class BookSpider(scrapy.Spider):
+    name = "book"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["http://books.toscrape.com/"]
+
+    def parse(self, response):
+        pass
