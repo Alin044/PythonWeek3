53271984f9ad83a143359b1230aeb0d4ce17dc61
Alin044 <alinalin03030@gmail.com>
2026-03-06 13:20:17 +0000

    handeling pagination

--- MyPythonProjects/WebScraping/books/books/spiders/book.py
+++ MyPythonProjects/WebScraping/books/books/spiders/book.py
@@ -1,6 +1,6 @@
 import scrapy
 
-from books.books.items import BooksItem
+from books.items import BooksItem
 
 class BookSpider(scrapy.Spider):
     name = "book"
@@ -11,8 +11,12 @@ class BookSpider(scrapy.Spider):
         for book in response.css("article.product_pod"):
             item = BooksItem()
             item["url"] = book.css("h3 > a::attr(href)").get()
-            item["title"] = book.css("h3 > a::attr(text)").get()
+            item["title"] = book.css("h3 > a::attr(title)").get()
             item["author"] = book.css(".price_color::text").get()
             yield item
 
-            
+        next_page = response.css("li.next > a::attr(href)").get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+
